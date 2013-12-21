ec64b40e24a8d4f4980e85bc1bbaf6dd729ea520
DSigmund <sigmund.dominik@googlemail.com>
2013-12-21 20:31:00 +0100

    Added Information for New FOrm

--- modules/mod_customers/php/listCustomers.php
+++ modules/mod_customers/php/listCustomers.php
@@ -1,4 +1,5 @@
 <?php
 //check if uid is allowed
+//formular for new cosutomer (dialog) is here
 //List customers (select * ) into table. js functions to show details, edit, delete, new
 //Table must be: sortable, filterable, groupable(if applicable)
